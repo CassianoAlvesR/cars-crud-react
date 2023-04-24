eba798f2908c5dec4d4e725966bb821ebb7f2422
CassianoAlvesR <cassianoalvescontato@gmail.com>
2023-04-24 20:50:27 -0300

    Iniciando Projeto Integracao CHATGPT

---
+++ Desktop/zap/go.mod
@@ -0,0 +1,3 @@
+module github.com/devfullcycle/fclx/zap
+
+go 1.20
---
+++ Desktop/zap/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type Message struct {
+	Role    string `json:"role"`
+	Content string `json:"content"`
+}
+
+type Request struct {
+	Model     string    `json:"model"`
+	Messages  []Message `json:"messages"`
+	MaxTokens int       `json:"max_tokens"`
+}
+
+type Response struct {
+	ID      string    `json:"model"`
+	Object  string    `json:"object"`
+	Created int       `json:"created"`
+	Choices []Choices `json:"choices"`
+}
+
+type Choices struct {
+	Index   int `json:"index"`
+	Message struct {
+		Role    string `json:"role"`
+		Content string `json:"content"`
+	} `json:"message"`
+}
+
+func GenerateGPTText(query string) (string, error) {
+	req := Request{
+		Model: "gpt-3.5-turbo",
+		Messages: []Message{
+			{
+				Role:    "user",
+				Content: query,
+			},
+		},
+		MaxTokens: 300,
+	}
+	reqJson, err := json.Marshal(req)
+	if err != nil {
+		return "", err
+	}
+
+	request, err := http.NewRequest("POST", "https://api.openai.com/v1/chat/completions", bytes.NewBuffer(reqJson))
+
+	if err != nil {
+		return "", err
+	}
+	request.Header.Set("Content-Type", "application/json")
+	request.Header.Set("Authorization", "Bearer sk-xxx")
+
+	response, err := http.DefaultClient.Do(request)
+	if err != nil {
+		return "", err
+	}
+	defer response.Body.Close()
+
+	respBody, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return "", err
+	}
+
+	var resp Response
+	err = json.Unmarshal(respBody, &resp)
+	if err != nil {
+		return "", err
+	}
+	return resp.Choices[0].Message.Content, nil
+}
+
+func main() {
+	fmt.Println("Hello, World")
+}
